0b9ba4a0164acab6f4de99dcb918de942df7e064
Madalina Antochi <mada_antochi@yahoo.com>
2017-07-18 11:26:14 +0300

    Added login and logout views

--- message/forms.py
+++ message/forms.py
@@ -1,11 +1,34 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 class RegisterForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'username', 'password']
+        fields = ['first_name', 'last_name', 'username', 'email', 'password']
+
+        #parola nu trebuie sa fie vizibila
         widgets = {
             "password": forms.PasswordInput
         }
+    retype_password=forms.CharField(widget=forms.PasswordInput)
+
+#validarea parolelor
+    def clean_retype_password (self):
+        password=self.cleaned_data['password']
+        retype_password=self.cleaned_data['retype_password']
+
+        if password!=retype_password:
+            raise ValidationError("The two password do not match")
+        return retype_password
+
+#salvarea datelor (in baza de date) cu parola criptata
+    def save(self, commit=True):
+        new_user=super(RegisterForm, self).save(commit=False)
+        new_user.set_password(self.cleaned_data['password'])
+        if commit:
+            new_user.save()
+        return  new_user
+
+
--- message/views.py
+++ message/views.py
@@ -5,7 +5,18 @@ from django.views.generic.edit import CreateView
 
 from message.forms import RegisterForm
 
+from django.views.generic.list import ListView
+
 
 class RegisterView(CreateView):
     form_class = RegisterForm
     template_name = 'register.html'
+
+    #ma duce pe pagina principala
+    success_url = "/"
+
+class TimelineView(ListView):
+    template_name = 'index.html'
+    def get_queryset(self):
+        # implement the logic
+        pass
